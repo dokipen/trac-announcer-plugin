a86f7bff5f9b1ca0e50e2f4e80e5d13db8428943
doki_pen <doki_pen@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-13 14:29:00 +0000

    Updates tests for new API
    git-svn-id: http://trac-hacks.org/svn/announcerplugin/trunk@7565 7322e99d-02ea-0310-aa39-e9a107903beb

--- announcer/tests/ticket_compat.py
+++ announcer/tests/ticket_compat.py
@@ -43,44 +43,30 @@ class StaticTicketSubscriberTestCase(unittest.TestCase):
         self.env.config.set('announcer', 'smtp_always_bcc', 'bu1, bu2, bu3')
         self.out = StaticTicketSubscriber(self.env)
 
-    def test_realms(self):
-        self.assertEquals(('*',), self.out.get_subscription_realms())
-        self.env.config.set('announcer', 'smtp_always_cc', None)
-        self.env.config.set('announcer', 'smtp_always_bcc', None)
-        self.assertEquals(tuple(), self.out.get_subscription_realms())
-
-    def test_categories(self):
-        self.assertEquals(('*',), self.out.get_subscription_categories('wiki'))
-        self.assertEquals(('*',), self.out.get_subscription_categories('ticket'))
-        self.env.config.set('announcer', 'smtp_always_cc', None)
-        self.env.config.set('announcer', 'smtp_always_bcc', None)
-        self.assertEquals(tuple(), self.out.get_subscription_categories('wiki'))
-        self.assertEquals(tuple(), self.out.get_subscription_categories('ticket'))
-
     def test_cc(self):
-        subs = [s for s in self.out.get_subscriptions_for_event(None)]
+        subs = [s for s in self.out.subscriptions(None)]
         self.assertTrue(('email', None, False, 'u1') in subs)
         self.assertTrue(('email', None, False, 'u2') in subs)
         self.assertTrue(('email', None, False, 'u3') in subs)
         self.env.config.set('announcer', 'smtp_always_bcc', None)
-        subs = [s for s in self.out.get_subscriptions_for_event(None)]
+        subs = [s for s in self.out.subscriptions(None)]
         self.assertTrue(('email', None, False, 'u1') in subs)
         self.assertTrue(('email', None, False, 'u2') in subs)
         self.assertTrue(('email', None, False, 'u3') in subs)
 
     def test_bcc(self):
-        subs = [s for s in self.out.get_subscriptions_for_event(None)]
+        subs = [s for s in self.out.subscriptions(None)]
         self.assertTrue(('email', None, False, 'bu1') in subs)
         self.assertTrue(('email', None, False, 'bu2') in subs)
         self.assertTrue(('email', None, False, 'bu3') in subs)
         self.env.config.set('announcer', 'smtp_always_cc', None)
-        subs = [s for s in self.out.get_subscriptions_for_event(None)]
+        subs = [s for s in self.out.subscriptions(None)]
         self.assertTrue(('email', None, False, 'bu1') in subs)
         self.assertTrue(('email', None, False, 'bu2') in subs)
         self.assertTrue(('email', None, False, 'bu3') in subs)
 
     def test_list_size(self):
-        subs = [s for s in self.out.get_subscriptions_for_event(None)]
+        subs = [s for s in self.out.subscriptions(None)]
         self.assertEquals(6, len(subs))
 
 def suite():
