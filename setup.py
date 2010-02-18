8325595136acf6faee8e232f8128c8fedcb92b93
doki_pen <doki_pen@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-18 05:22:06 +0000

    reformats setup.py
    git-svn-id: http://trac-hacks.org/svn/announcerplugin/trunk@7636 7322e99d-02ea-0310-aa39-e9a107903beb

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
             'announcer.resolvers.defaultdomain = announcer.resolvers.defaultdomain',
             'announcer.resolvers.sessionemail = announcer.resolvers.sessionemail',
             'announcer.resolvers.specified = announcer.resolvers.specified',
-    #        'announcer.subscribers.rulefilters = announcer.subscribers.rulefilters',
+            #'announcer.subscribers.rulefilters = announcer.subscribers.rulefilters',
             'announcer.subscribers.ticket_compat = announcer.subscribers.ticket_compat',
             'announcer.subscribers.ticket_components = announcer.subscribers.ticket_components',
             'announcer.subscribers.ticket_custom = announcer.subscribers.ticket_custom',
