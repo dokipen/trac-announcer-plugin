fec9991fe29eed7f9bdb1e4b964202801c5c6be3
doki_pen <doki_pen@7322e99d-02ea-0310-aa39-e9a107903beb>
2010-02-19 13:31:49 +0000

    makes legacy subscriber use settings
    Also updated documentation.
    
    git-svn-id: http://trac-hacks.org/svn/announcerplugin/trunk@7671 7322e99d-02ea-0310-aa39-e9a107903beb

--- announcer/subscribers/ticket_compat.py
+++ announcer/subscribers/ticket_compat.py
@@ -42,6 +42,7 @@ from trac.web.chrome import add_warning
 
 from announcer.api import IAnnouncementSubscriber, istrue
 from announcer.api import IAnnouncementPreferenceProvider
+from announcer.util.settings import BoolSubscriptionSetting
 
 class LegacyTicketSubscriber(Component):
     """Mimics Trac notification settings with added bonus of letting users
@@ -49,161 +50,111 @@ class LegacyTicketSubscriber(Component):
     """
     implements(IAnnouncementSubscriber, IAnnouncementPreferenceProvider)
     
-    always_notify_owner = BoolOption("announcer", "always_notify_owner", 'true', 
+    owner = BoolOption("announcer", "always_notify_owner", True,
         """The always_notify_owner option mimics the option of the same name 
-        in the notification section, except users can opt-out in their 
-        preferences. Used only if LegacyTicketSubscriber is enabled.""")
+        in the notification section, except users can override in their 
+        preferences.
+        """)
 
-    always_notify_reporter = BoolOption("announcer", "always_notify_reporter", 
-        'true', """The always_notify_reporter option mimics the option of the 
-        same name in the notification section, except users can opt-out in 
-        their preferences. Used only if LegacyTicketSubscriber is enabled.""")
+    reporter = BoolOption("announcer", "always_notify_reporter", True, 
+        """The always_notify_reporter option mimics the option of the 
+        same name in the notification section, except users can override in 
+        their preferences.
+        """)
 
-    always_notify_updater = BoolOption("announcer", "always_notify_updater", 
-        'true', """The always_notify_updater option mimics the option of the 
-        same name in the notification section, except users can opt-out in 
-        their preferences. Used only if LegacyTicketSubscriber is enabled.""")
+    updater = BoolOption("announcer", "always_notify_updater", True, 
+        """The always_notify_updater option mimics the option of the 
+        same name in the notification section, except users can override in 
+        their preferences. 
+        """)
 
-    always_notify_component_owner = BoolOption("announcer", 
-            "always_notify_component_owner", 'true',
-            """Whether or not to notify the owner of the ticket's 
-            component.""")
-        
+    component_owner = BoolOption("announcer", "always_notify_component_owner", 
+        True,
+        """Whether or not to notify the owner of the ticket's component.  The
+        user can override this setting in their preferences.
+        """)
+    
     def get_announcement_preference_boxes(self, req):
         yield "legacy", "Ticket Subscriptions"
 
     def render_announcement_preference_box(self, req, panel):
+        settings = self._settings()
         if req.method == "POST":
-            for attr in ('component_owner', 'owner', 'reporter', 'updater'):
-                val = req.args.get('legacy_notify_%s'%attr) == 'on' 
-                val = val and '1' or '0'
-                req.session['announcer_legacy_notify_%s'%attr] = val
+            for attr, setting in settings.items():
+                setting.set_user_setting(req.session, 
+                        req.args.get('legacy_notify_%s'%attr), save=False)
+            req.session.save()
 
-        # component
-        component = req.session.get('announcer_legacy_notify_component_owner')
-        if component is None:
-            component = self.always_notify_component_owner
-        else:
-            component = component == u'1'
-
-        # owner
-        owner = req.session.get('announcer_legacy_notify_owner')
-        if owner is None:
-            owner = self.always_notify_owner
-        else:
-            owner = owner == u'1'
-
-        # reporter
-        reporter = req.session.get('announcer_legacy_notify_reporter')
-        if reporter is None:
-            reporter = self.always_notify_reporter
-        else:
-            reporter = reporter == u'1'
-
-        # updater
-        updater = req.session.get('announcer_legacy_notify_updater')
-        if updater is None:
-            updater = self.always_notify_updater
-        else:
-            updater = updater == u'1'
-
-        return "prefs_announcer_legacy.html", dict(
-            data=dict(
-                component=component,
-                owner=owner,
-                reporter=reporter,
-                updater=updater
-            )    
-        )
+        vars = {}
+        for attr, setting in settings.items():
+            vars[attr] = setting.get_user_setting(req.session.sid)[0]
+        return "prefs_announcer_legacy.html", dict(data=vars)
 
     def subscriptions(self, event):
         if event.realm == "ticket":
             if event.category in ('created', 'changed', 'attachment added'):
+                settings = self._settings()
                 ticket = event.target
-                subs = filter(lambda a: a, (
-                    self._always_notify_component_owner(ticket),
-                    self._always_notify_ticket_owner(ticket),
-                    self._always_notify_ticket_reporter(ticket), 
-                    self._always_notify_ticket_updater(event, ticket)
-                ))
-                for s in subs:
-                    yield s
-    def _always_notify_component_owner(self, ticket):
+                for attr, setting in settings.items():
+                    getter = self.__getattribute__('_get_%s'%attr)
+                    subscription = getter(event, ticket, setting)
+                    if subscription:
+                        yield subscription
+                    
+    # A group of helpers for getting each type of subscriber
+    def _get_component_owner(self, event, ticket, setting):
         try:
             component = model.Component(self.env, ticket['component'])
             if component.owner:
-                notify = self._check_user_setting('notify_component_owner', 
-                        component.owner)
-                if notify is None:
-                    notify = self.always_notify_component_owner
-                if notify:
-                    self._log_sub(component.owner, True, 
-                            'always_notify_component_owner')
+                if setting.get_user_setting(component.owner)[0]:
+                    self._log_sub(component.owner, True, setting.name)
                     return ('email', component.owner, True, None)
         except ResourceNotFound, message:
             self.log.warn(_("LegacyTicketSubscriber couldn't add " \
                     "component owner because component was not found, " \
                     "message: '%s'"%(message,)))
 
-    def _always_notify_ticket_owner(self, ticket):
+    def _get_owner(self, event, ticket, setting):
         if ticket['owner']:
-            notify = self._check_user_setting('notify_owner', ticket['owner'])
-            if notify is None:
-                notify = self.always_notify_owner
-            if notify: 
+            if setting.get_user_setting(ticket['owner'])[0]:
                 owner = ticket['owner']
                 if '@' in owner:
                     name, authenticated, address = None, False, owner
                 else:
                     name, authenticated, address = owner, True, None
-                self._log_sub(owner, authenticated, 'always_notify_owner')
+                self._log_sub(owner, authenticated, setting.name)
                 return ('email', name, authenticated, address)
         
-    def _always_notify_ticket_reporter(self, ticket):
+    def _get_reporter(self, event, ticket, setting):
         if ticket['reporter']:
-            notify = self._check_user_setting('notify_reporter', ticket['reporter'])
-            if notify is None:
-                notify = self.always_notify_reporter
-            if notify:
+            if setting.get_user_setting(ticket['reporter'])[0]:
                 reporter = ticket['reporter']
                 if '@' in reporter:
                     name, authenticated, address = None, False, reporter
                 else:
                     name, authenticated, address = reporter, True, None
-                self._log_sub(reporter, authenticated, 'always_notify_reporter')
+                self._log_sub(reporter, authenticated, setting.name)
                 return ('email', name, authenticated, address)
 
-    def _always_notify_ticket_updater(self, event, ticket):
+    def _get_updater(self, event, ticket, setting):
         if event.author:
-            notify = self._check_user_setting('notify_updater', event.author)
-            if notify is None:
-                notify = self.always_notify_updater
-            if notify:
-                self._log_sub(event.author, True, 'always_notify_updater')
+            if setting.get_user_setting(event.author)[0]:
+                self._log_sub(event.author, True, setting.name)
                 return ('email', event.author, True, None)
     
     def _log_sub(self, author, authenticated, rule):
-        "Log subscriptions"
+        """Log subscriptions"""
         auth = authenticated and 'authenticated' or 'not authenticated'
-        self.log.debug(_("LegacyTicketSubscriber added '%s " \
-            "(%s)' because of rule: %s"%(author, auth, rule)))
+        self.log.debug("""LegacyTicketSubscriber added '%s (%s)' because of
+            rule: always_notify_%s
+            """%(author, auth, rule))
         
-    def _check_user_setting(self, preference, sid):
-        """Check the user's selection.  None means 
-        they haven't selected anything."""
-        db = self.env.get_db_cnx()
-        cursor = db.cursor()
-        cursor.execute("""
-            SELECT value 
-              FROM session_attribute
-             WHERE sid=%s
-               AND authenticated=1
-               AND name=%s
-        """, (sid, 'announcer_legacy_' + preference))
-        result = cursor.fetchone()
-        if result:
-            return result[0] == '1'
-        return None
+    def _settings(self):
+        ret = {}
+        for p in ('component_owner', 'owner', 'reporter', 'updater'):
+            default = self.__getattribute__(p)
+            ret[p] = BoolSubscriptionSetting(self.env, "ticket_%s"%p, default)
+        return ret
 
 class CarbonCopySubscriber(Component):
     """Carbon copy subscriber for cc ticket field."""
